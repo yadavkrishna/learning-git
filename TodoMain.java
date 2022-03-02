28c74bf52532bfe422be6a3720868d94a020e6c4
yadavkrishna <krishnakmr560@gmail.com>
2022-03-02 16:52:31 +0530

    Try to learn basic of Todo create basic class of todo and add todo but not efficient

---
+++ TodoMain.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class TodoMain {
+    public static void main(String[] args) {
+    System.out.println("*** Welcome to TODO ***\n");
+    ArrayList<TodoEg> al=new ArrayList<>();
+
+        //create todo
+        TodoEg todo=new TodoEg("to complete todo App", new Date());
+        TodoEg t1=new TodoEg("Also learn theory for exam", new Date());
+        
+
+    al.add(todo);
+    al.add(t1);
+
+    //print data using simple sysout
+    // System.out.println("Content of todo: \n"+al); 
+    
+    //using foreach loop
+    for (TodoEg todoEg : al) {
+        System.out.println(todoEg);
+    }
+   
+    }
+}
